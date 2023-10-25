ab570a19cf9cbd9bed2e2b7a8ea8801fc6d7a8a8
luciamicheloud <123605664+luciamicheloud@users.noreply.github.com>
2023-10-25 12:07:11 -0300

    chat gpt mi dios

--- etiniaSoftCommerce/client/src/components/navBar/NavBar.jsx
+++ etiniaSoftCommerce/client/src/components/navBar/NavBar.jsx
@@ -1,1 +1,14 @@
-//vvvv
+import React from 'react';
+
+function NavBar(props) {
+  return (
+    <div className="nav-bar">
+      <button>Usuario</button>
+      <button>Home</button>
+      <button>Carrito</button>
+      <button>Configuraciones</button>
+    </div>
+  );
+}
+
+export default NavBar;
--- etiniaSoftCommerce/client/src/components/pagination/Pagination.jsx
+++ etiniaSoftCommerce/client/src/components/pagination/Pagination.jsx
@@ -1,1 +1,13 @@
-//fhhghghgh
+import React from 'react';
+
+function Pagination(props) {
+  return (
+    <div className="pagination">
+      <button>&lt;</button> {/* Flecha izquierda */}
+      {/* Renderiza los puntos y la página actual aquí */}
+      <button>&gt;</button> {/* Flecha derecha */}
+    </div>
+  );
+}
+
+export default Pagination;
