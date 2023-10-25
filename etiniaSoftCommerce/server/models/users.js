50c2b356fa18383273b841fd9c9e0840dde2c891
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-10-24 19:11:22 -0600

    update users.js
    la tabla de users quedaria asi

---
+++ etiniaSoftCommerce/server/models/users.js
@@ -0,0 +1,49 @@
+const {DataTypes} = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      last_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      phone_number:{
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+      },
+      address:{
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      admin:{
+        type: DataTypes.BOOLEAN,
+            allowNull: false,
+      },
+      employee:{
+        type: DataTypes.BOOLEAN,
+            allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        isEmail: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        is: [/\d/],
+      },
+    },
+    {timestamps: false}
+  );
+};
