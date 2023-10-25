e78a2eaad40a155b06442b82d5d44c76cf0d38b5
santi24qm1999 <santiagoqm99.9@gmail.com>
2023-10-24 21:04:32 -0500

    creando la arquitectura del server

---
+++ etiniaSoftCommerce/server/src/routes/index.js
@@ -0,0 +1,12 @@
+const {Router}= require("express");
+//const importModelo = require("./modelo");
+
+
+
+const indexRouter = Router();
+
+
+//indexRouter.use("/***modelo***", modelo);
+
+
+module.exports = indexRouter;
