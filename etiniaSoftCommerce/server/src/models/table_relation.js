1dd08d7af395a8094a858636951123b5df519989
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-10-25 20:16:00 -0600

    [FIX] table_relation

--- etiniaSoftCommerce/server/src/models/table_relation.js
+++ etiniaSoftCommerce/server/src/models/table_relation.js
@@ -1,5 +1,8 @@
 const User = require('./users');
 const Purchase = require('./purchases');
+const Reviews = require('./reviews')
 
 User.hasMany(Purchase, { foreignKey: 'userId' });
 Purchase.belongsTo(User, { foreignKey: 'userId' });
+User.hasMany(Reviews, { foreingKey: 'userId'});
+Reviews.belongsTo(User, { foreignKey: 'userId'});
