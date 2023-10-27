52820ee9d76ccc1717f2ab0c4092377a81e0307a
luciamicheloud <123605664+luciamicheloud@users.noreply.github.com>
2023-10-27 16:51:01 -0300

    estilos home, nav, filters

--- etiniaSoftCommerce/client/src/components/navBar/NavBar.jsx
+++ etiniaSoftCommerce/client/src/components/navBar/NavBar.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Carrito from '../../assets/png/Carrito.png';
+import Home from '../../assets/png/Home.png';
+import Usuario from '../../assets/png/Usuario.png';
+import Configuraciones from '../../assets/png/Configuraciones.png';
+import styles from './NavBar.module.css'; // Importa los estilos CSS
 
 function NavBar(props) {
   return (
-    <div className="nav-bar">
+    <div className={styles.navbar}>
       <button>
-      <Link to="/user/:id">Usuario</Link>
+        <Link to="/user/:id"><img className={styles.Usuario} src={Usuario} alt="Usuario" /></Link>
       </button>
 
       <button>
-        <Link to="/home">Home</Link>
+        <Link to="/home"><img className={styles.Home} src={Home} alt="Home" /></Link>
       </button>
 
       <button>
-        <Link to="/carrito">Carrito</Link>
+        <Link to="/carrito"><img className={styles.Carrito} src={Carrito} alt="Carrito" /></Link>
       </button>
 
       <button>
-      <Link to="/home">Configuraciones</Link>
+        <Link to="/home"><img className={styles.Configuraciones} src={Configuraciones} alt="Configuraciones" /></Link>
       </button>
     </div>
   );
--- etiniaSoftCommerce/client/src/pages/home/Home.jsx
+++ etiniaSoftCommerce/client/src/pages/home/Home.jsx
@@ -3,8 +3,7 @@ import Header from '../../components/header/Header';
 import CardContainer from '../../components/cardsContainer/CardsContainer';
 import NavBar from '../../components/navBar/NavBar';
 import products from "../../utils/db.json"
-import SearchBar from '../../components/searchBar/SearchBar';
-import Select from '../../components/sizeSelector/SizeSelector';
+import Filters from '../../components/filters/Filters';
 
 function Home(props) {
 
@@ -40,15 +39,14 @@ function Home(props) {
 
   return (
     <div className="home">
-      <Header />
-      <Select name={"Gender"} options={genderOpt} handleChange={handleChange} state={null}/>
-      <Select name={"Category"} options={categoryOpt} handleChange={handleChange} state={null}/>
-      <Select name={"Color"} options={colorOpt} handleChange={handleChange} state={null}/>
-      <Select name={"Sale"} options={saleOpt} handleChange={handleChange} state={null}/>
-      <Select name={"Size"} options={sizeOpt} handleChange={handleChange} state={null}/>
-      <Select name={"Price"} options={PriceOpt} handleChange={handleChange} state={null}/>
-      <h2>Bienvenido al Home</h2>
       <NavBar />
+      <Header />
+      <Filters name={"Gender"} options={genderOpt} handleChange={handleChange} state={null}/>
+      <Filters name={"Category"} options={categoryOpt} handleChange={handleChange} state={null}/>
+      <Filters name={"Color"} options={colorOpt} handleChange={handleChange} state={null}/>
+      <Filters name={"Sale"} options={saleOpt} handleChange={handleChange} state={null}/>
+      <Filters name={"Size"} options={sizeOpt} handleChange={handleChange} state={null}/>
+      <Filters name={"Price"} options={PriceOpt} handleChange={handleChange} state={null}/>
       <CardContainer products={products}/>
     </div>
   );
