cbf64907838cdffb79e0726cd038ebbe1ec5ff2a
Jessuss <jesusacosta3321@gmail.com>
2023-10-26 21:55:45 -0300

    "Funcionalidad a SearchBar

--- etiniaSoftCommerce/client/src/components/searchBar/SearchBar.jsx
+++ etiniaSoftCommerce/client/src/components/searchBar/SearchBar.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useState } from 'react';
 
 function SearchBar(props) {
+  const [name, setName] = useState("");
+
+  const handleChange = (event)=>{
+    setName(event.target.value)
+    console.log(name)
+ };
+
+ const handleSearch = (event)=>{
+  setName("");
+}
+
   return (
     <div className="search-bar">
-      {/* Aquí debes implementar la barra de búsqueda */}
+      <input type="search" onChange={handleChange} value={name} placeholder="Search for a product..." />
+      <button onClick={handleSearch}>Buscar</button>
     </div>
   );
 }
