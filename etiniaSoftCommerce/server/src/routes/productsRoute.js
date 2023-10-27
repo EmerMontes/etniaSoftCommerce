cc6c1b9f90d1c84521ddcb7ed17714d4912b6a15
santi24qm1999 <santiagoqm99.9@gmail.com>
2023-10-26 20:49:02 -0500

    [feat]: rutas terminadas en el back

--- etiniaSoftCommerce/server/src/routes/productsRoute.js
+++ etiniaSoftCommerce/server/src/routes/productsRoute.js
@@ -1,9 +1,10 @@
 productsRouter = require("express").Router();
-const {getProductseHandler, getIdHandler, getProductsByName, createProductsHandler,deleteProductsHandler,updateProductsHandler} = require("../handlers/productsHandler")
+const {getProductseHandler, getIdHandler, getProductsByName, createProductsHandler,deleteProductsHandler,updateProductsHandler,handleProductFilters} = require("../handlers/productsHandler")
 
 productsRouter.get("/", getProductseHandler);
 productsRouter.get("/:id", getIdHandler);
 productsRouter.get("/name/:name",getProductsByName);
+productsRouter.get("/filter/:filter", handleProductFilters);
 productsRouter.post("/",createProductsHandler);
 productsRouter.delete("/delete/:id", deleteProductsHandler);
 productsRouter.put("/put/:id", updateProductsHandler);
