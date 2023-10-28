ce8dfa8684451e13bee4bae0cc4d0141dcbc7d15
luciamicheloud <123605664+luciamicheloud@users.noreply.github.com>
2023-10-28 13:36:20 -0300

    NavBar user login

--- etiniaSoftCommerce/client/src/components/navBar/NavBar.jsx
+++ etiniaSoftCommerce/client/src/components/navBar/NavBar.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 import Carrito from '../../assets/png/Carrito.png';
 import Home from '../../assets/png/Home.png';
 import Usuario from '../../assets/png/Usuario.png';
 import Configuraciones from '../../assets/png/Configuraciones.png';
-import styles from './NavBar.module.css'; // Importa los estilos CSS
+import styles from './NavBar.module.css';
 
 function NavBar(props) {
+  const navigate = useNavigate();
+
+  const handleUserClick = () => {
+    const isUserLoggedIn = false; // Reemplazar con lógica real
+
+    if (isUserLoggedIn) {
+      navigate('/user/:id'); // Usuario registrado, redirige a la página de detalles de usuario.
+    } else {
+      navigate('/user'); // Usuario no registrado, redirige a la página de inicio de sesión.
+    }
+  };
+
   return (
     <div className={styles.navbar}>
-      <button>
-        <Link to="/user/:id"><img className={styles.Usuario} src={Usuario} alt="Usuario" /></Link>
+      <button onClick={handleUserClick}>
+        <img className={styles.Usuario} src={Usuario} alt="Usuario" />
       </button>
 
       <button>
