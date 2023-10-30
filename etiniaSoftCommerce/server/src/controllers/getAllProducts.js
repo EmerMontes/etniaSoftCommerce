a44968a96daf820ea15e7cf97fd3385730b125c5
JMRodriguezHurtado <jmrodhurtado1989@hotmail.com>
2023-10-29 21:39:33 -0600

    [FIX] limpiando un poco de codigo

--- etiniaSoftCommerce/server/src/controllers/getAllProducts.js
+++ etiniaSoftCommerce/server/src/controllers/getAllProducts.js
@@ -1,25 +1,10 @@
 const { Products } = require("../db");
 
 const getAllProducts = async (req, res, next) => {
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 10;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
   const results = {};
 
-  console.log("hola mundo");
   try {
-    const count = await Products.count();
-
-    results.info = {
-      page: page,
-      limit: 1,
-    };
-
-    const products = await Products.findAll({
-      limit: limit,
-      offset: startIndex,
-    });
+    const products = await Products.findAll();
 
     results.results = products;
     res.paginatedResults = results;
