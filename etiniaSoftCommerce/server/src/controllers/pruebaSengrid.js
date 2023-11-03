cd0e86ee52b9725c345f69938e294e047abbda16
EdueliasXez <edueliasxez@gmail.com>
2023-11-03 15:29:53 -0300

    para deploy

---
+++ etiniaSoftCommerce/server/src/controllers/pruebaSengrid.js
@@ -0,0 +1,22 @@
+//require('dotenv').config();
+
+//const sgMail = require('@sendgrid/mail');
+//sgMail.setApiKey('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');
+////const msg = {
+//  to: 'edueliasxez@gmail.com', // Cambia a tu destinatario
+ // from: 'clickyticketg18pf@gmail.com', // Cambia a tu remitente verificado
+//  subject: 'Enviar con SendGrid es divertido',
+//  text: 'y fácil de hacer en cualquier lugar, incluso con Node.js',
+//  html: '<strong>y fácil de hacer en cualquier lugar, incluso con Node.js</strong>',
+//};
+//sgMail
+//  .send(msg)
+//  .then(() => {
+//    console.log('Email enviado')
+//  })
+//  .catch((error) => {
+//    console.error(error)
+//  });//
+
+// como enviar un email con sendgrid
+
