cb3c9c4177e4346e7c6cff2631bd122c9c3848e9
Joako <joako12409@gmail.com>
2023-11-07 16:59:19 -0300

    ADD actions

--- etiniaSoftCommerce/client/src/redux/actions.js
+++ etiniaSoftCommerce/client/src/redux/actions.js
@@ -11,15 +11,35 @@ export const GET_USERS_BY_NAME = "GET_USERS_BY_NAME";
 export const GET_PRODUCTS_BY_NAME = "GET_PRODUCTS_BY_NAME";
 export const ADD_FAVORITES = "ADD_FAVORITES";
 export const GET_BY_ID = "GET_BY_ID";
+export const GET_ALL_COMMENTS="GET_ALL_COMMENTS";
+export const GET_EMPRESA="GET_EMPRESA";
+export const GET_CUENTAS="GET_CUENTAS";
+export const GET_MEDIOPAGO="GET_MEDIOPAGO";
+export const GET_LOGISTICA="GET_LOGISTICA";
 //routes Delete
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
+export const DELETE_COMMENT="DELETE_COMMENT";
+export const DELETE_EMPRESA="DELETE_EMPRESA";
+export const DELETE_CUENTAS="DELETE_CUENTAS";
+export const DELETE_MEDIOPAGO="DELETE_MEDIOPAGO";
+export const DELETE_LOGISTICA="DELETE_LOGISTICA";
 //Routes Post
 export const CREATE_PRODUCT = "CREATE_PRODUCT";
 export const CREATE_USER = "CREATE_USER";
 export const RESTORE_PRODUCT = "RESTORE_PRODUCT";
+export const CREATE_COMMENT="CREATE_COMMENT";
+export const CREATE_EMPRESA="CREATE_EMPRESA";
+export const CREATE_CUENTAS="CREATE_CUENTAS";
+export const CREATE_MEDIOPAGO="CREATE_MEDIOPAGO";
+export const CREATE_LOGISTICA="CREATE_LOGISTICA";
 //routes Put
 export const UPDATE_USER = "UPDATE_USER";
 export const UPDATE_PRODUCT="UPDATE_PRODUCT";
+export const UPDATE_COMMENT = "UPDATE_COMMENT";
+export const UPDATE_EMPRESA="UPDATE_EMPRESA";
+export const UPDATE_CUENTAS="UPDATE_CUENTAS";
+export const UPDATE_MEDIOPAGO="UPDATE_MEDIOPAGO";
+export const UPDATE_LOGISTICA="UPDATE_LOGISTICA";
 //Filters
 export const GET_FILTER_GENDER = "GET_FILTER_GENDER";
 export const GET_FILTER_CATEGORY = "GET_FILTER_CATEGORY";
@@ -43,6 +63,92 @@ export const REGISTER_USER = "REGISTER_USER";
 
 //const URL = "http://localhost:3001";
 const URL = "https://etniasoftcommerce.up.railway.app";
+
+export function getCuentas(){
+  return async function(dispatch){
+    const cuentasInfo= await axios.get(`/cuentas`);
+    dispatch({
+      type:GET_CUENTAS,
+      payload:cuentasInfo.data.results,
+    })
+  }
+}
+export function getEmpresa(){
+  return async function(dispatch){
+    const empresaInfo= await axios.get(`/empresa`);
+    dispatch({
+      type:GET_EMPRESA,
+      payload:empresaInfo.data.results,
+    })
+  }
+}
+export function getMedioPago(){
+  return async function(dispatch){
+    const mediopagoInfo= await axios.get(`/mediopago`);
+    dispatch({
+      type:GET_MEDIOPAGO,
+      payload:mediopagoInfo.data.results,
+    })
+  }
+}
+export function getLogistica(){
+  return async function(dispatch){
+    const logisticaInfo= await axios.get(`/logistica`);
+    dispatch({
+      type:GET_LOGISTICA,
+      payload:logisticaInfo.data.results,
+    })
+  }
+}
+
+export function getAllProducts() {
+  return async function (dispatch) {
+    const productsInfo = await axios.get(`${URL}/products`);
+    console.log(productsInfo);
+    dispatch({
+      type: GET_ALL_PRODUCTS,
+      payload: productsInfo.data.results,
+    });
+  };
+}
+
+export function getAllComments(id){
+  return async function(dispatch){
+    const commentsInfo=await axios.get(`/comments/${id}`)
+    dispatch({
+      type:GET_ALL_COMMENTS,
+      payload:commentsInfo.data
+    })
+  }
+}
+export function createComment(payload){
+  return async function(dispatch){
+    const info= await axios.post(`/comments`,payload)
+    dispatch({
+      type:CREATE_COMMENT,
+      payload:info.data
+    })
+  }
+}
+
+export function updateComment(payload){
+  return async function(dispatch){
+    const info =await axios.put(`/comments`,payload)
+    dispatch({
+      type:UPDATE_COMMENT,
+      payload:info.data
+    })
+  }
+}
+export function deleteComment(idUser, idProduct){
+  return async function (dispatch){
+    const deletedComment= await axios.delete(`/comments/${idUser}/${idProduct}`)
+    dispatch({
+      type:DELETE_COMMENT,
+      payload:deletedComment.data
+    })
+  }
+}
 
 
 export function registerUser(payload) {
@@ -205,16 +311,7 @@ export function getAllSelects() {
   };
 }
 
-export function getAllProducts() {
-  return async function (dispatch) {
-    const productsInfo = await axios.get(`${URL}/products`);
-    console.log(productsInfo);
-    dispatch({
-      type: GET_ALL_PRODUCTS,
-      payload: productsInfo.data.results,
-    });
-  };
-}
+
 
 export function getAddFavorites(product) {
   return async (dispatch) => {
--- etiniaSoftCommerce/client/src/redux/reducer.js
+++ etiniaSoftCommerce/client/src/redux/reducer.js
@@ -19,6 +19,10 @@ import {
   USER_LOGOUT,
   GET_ALL_SELECTS,
   LOCALSTORAGE,
+  CREATE_COMMENT,
+  UPDATE_COMMENT,
+  GET_ALL_COMMENTS,
+  DELETE_COMMENT,
 
  // REMOVE_SHIPPING,
   //UPDATE_SHIPPING,
@@ -31,6 +35,7 @@ import {
 const initialState = {
   allProducts: [],
   productDetail: [],
+  productComments: [],
   allFavorites: [],
   productShow: [],
   indexProductShow: [],
@@ -51,6 +56,23 @@ const reducer = (state = initialState, action) => {
         ...state,
         allProducts: action.payload,
       };
+    case GET_ALL_COMMENTS:
+      return{
+        ...state,
+        reportedComments:action.payload
+      }
+    case CREATE_COMMENT:
+      return action.payload
+    case UPDATE_COMMENT:
+      return{
+        ...state,
+        productComments:action.payload
+      }      
+     case DELETE_COMMENT:
+      return{
+        ...state,
+        productComments:action.payload
+      } 
     case GET_ALL_PRODUCTS:
       return {
         ...state,
